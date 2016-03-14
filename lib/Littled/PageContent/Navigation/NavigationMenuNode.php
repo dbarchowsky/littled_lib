b2c10028bd45f5082053d7217bfd7635e974cf6f
Damien Barchowsky <dbarchowsky@gmail.com>
2016-03-14 11:41:11 -0700

    Adjusts paramter order for NavigationMenuNode() #34

--- lib/Littled/PageContent/Navigation/NavigationMenuNode.php
+++ lib/Littled/PageContent/Navigation/NavigationMenuNode.php
@@ -40,8 +40,8 @@ class NavigationMenuNode
 	/**
 	 * Class constructor.
 	 * @param string $label Text to display for this item within the navigation menu.
-	 * @param string $title Title attribute value for the node element.
 	 * @param string $url (Optional) URL where the menu item will link to.
+	 * @param string $title Title attribute value for the node element.
 	 * @param string $target (Optional) Target window for the link. Defaults to the same window.
 	 * @param integer $level (Optional) Indentation level of the menu item.
 	 * @param string $dom_id  (Optional) Sets the id attribute of the menu item element.
@@ -49,7 +49,7 @@ class NavigationMenuNode
 	 * @throws ConfigurationUndefinedException
 	 * @throws ResourceNotFoundException
 	 */
-	function __construct ( $label=null, $url=null, $target=null, $title='', $level=0, $dom_id=null, $attributes=null)
+	function __construct ( $label=null, $url=null, $title=null, $target=null, $level=0, $dom_id=null, $attributes=null)
 	{
 		if (!defined('LITTLED_TEMPLATE_DIR')) {
 			throw new ConfigurationUndefinedException("LITTLED_TEMPLATE_DIR not defined in app settings.");
