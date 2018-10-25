e662576ed74b70e7edbaddfac0e2e25c4526f805
Damien Barchowsky <dbarchowsky@gmail.com>
2018-10-25 15:26:43 -0700

    Added \Littled\Request\DateInput module

---
+++ lib/Littled/Request/DateInput.php
@@ -0,0 +1,107 @@
+<?php
+namespace Littled\Request;
+
+
+use Littled\Exception\ContentValidationException;
+
+/**
+ * Class DateInput
+ * @package Littled\Request
+ */
+class DateInput extends StringInput
+{
+	/**
+	 * DateInput constructor.
+	 * @param string $label Input label
+	 * @param string $param value of the name attribute of the input
+	 * @param boolean[optional] $required Flag indicating if this form data is required. Defaults to FALSE.
+	 * @param string[optional] $value Initial value of the input. Defaults to NULL.
+	 * @param int $size_limit[optional] Maximum size in bytes of the value when it is stored in the database (for strings). Defaults to 0.
+     * @param int[optional] $index Position of this form input within a series of similar inputs.
+	 */
+	function __construct(string $label, string $param, bool $required = false, $value = null, int $size_limit = 20, ?int $index=null)
+	{
+		parent::__construct($label, $param, $required, $value, $size_limit, $index);
+	}
+
+	/**
+	 * Validates the date value.
+	 * @param string $date_format Date format to apply to the date value.
+	 * @throws ContentValidationException Date value is missing when required or is in an unrecognized format.
+	 */
+	public function validate( $date_format='Y-m-d H:i:00' )
+	{
+		if ($this->required===true && ($this->value===null || strlen($this->value) < 1)) {
+			throw new ContentValidationException("{$this->label} is required.");
+		}
+		if (strlen($this->value) > $this->sizeLimit) {
+			throw new ContentValidationException("{$this->label} is limited to {$this->sizeLimit} character".(($this->sizeLimit!=1)?("s"):("")).".");
+		}
+		$this->setDateValue($date_format);
+	}
+
+	/**
+	 * Returns a string to use to save the object's value to a datbase record.
+	 * @param \mysqli $mysqli MySQLi connection to use for its escape_string() routine.
+	 * @return string Escaped value.
+	 */
+	public function escapeSQL($mysqli)
+	{
+        if ($this->value===null || $this->value=="") {
+            return ("NULL");
+        }
+        $ts = strtotime($this->value);
+        if ($ts !== false) {
+            $date_string = date("Y-m-d H:i:s",$ts);
+        }
+        else {
+            $date = \DateTime::createFromFormat('d/m/Y', $this->value);
+            if ($date!==false) {
+                $date_string = $date->format('Y-m-d');
+            }
+            else {
+                /* maybe it's in YYYY-MM-DD format, just send it back whatever it is */
+                $date_string = $this->value;
+            }
+        }
+        return("'".$mysqli->real_escape_string($date_string)."'");
+	}
+
+	/**
+	 * Converts the current value of the object to a standard date format.
+	 * @param string $date_format Date format to apply to the current value of the object.
+	 * @throws ContentValidationException Current value not a valid date value.
+	 */
+	protected function setDateValue( $date_format='Y-m-d H:i:00' )
+	{
+		$valid = (strtotime($this->value)!==false);
+		if (!$valid) {
+			$valid = (\DateTime::createFromFormat('d/m/Y', $this->value) !== false);
+		}
+		if (!$valid) {
+			$valid = (\DateTime::createFromFormat('Y-m-d', $this->value) !== false);
+		}
+		if (!$valid) {
+			throw new ContentValidationException("{$this->label} is not in a recognized date format.");
+		}
+		if ($date_format !== null) {
+			$this->value = date($date_format, strtotime($this->value));
+		}
+	}
+
+	/**
+	 * Assigns a value to the object after parsing the value so it is in a workable format.
+	 * @param string $value Value to assign to the object.
+	 * @param string $date_format Format to apply to the value.
+	 */
+	public function setInputValue($value, $date_format='Y-m-d')
+	{
+		parent::setInputValue($value);
+		try {
+			$this->setDateValue('Y-m-d');
+		}
+		catch(ContentValidationException $ex) {
+			$this->value = '';
+		}
+	}
+}
---
+++ tests/Request/DateInputTest.php
@@ -0,0 +1,151 @@
+<?php
+namespace Littled\Tests\Request;
+
+
+use Littled\Database\MySQLConnection;
+use Littled\Exception\ContentValidationException;
+use Littled\Request\DateInput;
+use PHPUnit\Framework\TestCase;
+
+
+class DateInputTest extends TestCase
+{
+    /** @var DateInput Test DateInput object. */
+    public $obj;
+    /** @var MySQLConnection Test database connection. */
+    public $conn;
+
+    public function setUp()
+    {
+        $this->obj = new DateInput("Test date", 'p_date');
+        $this->conn = new MySQLConnection();
+    }
+
+    /**
+     * @throws \Littled\Exception\ContentValidationException
+     */
+    public function testValidateValidValues()
+    {
+        $this->obj->required = true;
+        $this->obj->value = '1/15/2018';
+        $this->obj->validate();
+        $this->assertEquals('2018-01-15 00:00:00', $this->obj->value);
+
+        $this->obj->value = '12/31/1999';
+        $this->obj->validate();
+        $this->assertEquals('1999-12-31 00:00:00', $this->obj->value);
+
+        $this->obj->value = '1/1/2094';
+        $this->obj->validate();
+        $this->assertEquals('2094-01-01 00:00:00', $this->obj->value);
+
+        $this->obj->value = '2018-02-14';
+        $this->obj->validate();
+        $this->assertEquals('2018-02-14 00:00:00', $this->obj->value);
+
+        $this->obj->value = '01/01/2004';
+        $this->obj->validate();
+        $this->assertEquals('2004-01-01 00:00:00', $this->obj->value);
+
+        $this->obj->value = '11/01/2020';
+        $this->obj->validate();
+        $this->assertEquals('2020-11-01 00:00:00', $this->obj->value);
+
+	    $this->obj->value = '11-1-2020';
+	    $this->obj->validate();
+	    $this->assertEquals('2020-01-11 00:00:00', $this->obj->value);
+
+	    $this->obj->value = 'May 13, 1980';
+	    $this->obj->validate();
+	    $this->assertEquals('1980-05-13 00:00:00', $this->obj->value);
+    }
+
+    public function testSetInputValue()
+    {
+    	$m = date('m');
+    	$d = date('d');
+
+    	$this->obj->setInputValue('');
+    	$this->assertEquals('', $this->obj->value);
+
+	    $this->obj->setInputValue('1/1/2018');
+	    $this->assertEquals('2018-01-01', $this->obj->value);
+
+	    $this->obj->setInputValue('2018-01-01');
+	    $this->assertEquals('2018-01-01', $this->obj->value);
+
+	    $this->obj->setInputValue('June 13, 1969');
+	    $this->assertEquals('1969-06-13', $this->obj->value);
+
+	    $this->obj->setInputValue('dfdfdfd');
+	    $this->assertEquals('', $this->obj->value);
+
+	    $this->obj->setInputValue('7269');
+	    $this->assertEquals("7269-{$m}-{$d}", $this->obj->value);
+    }
+
+    public function testValidateMissingDateValue()
+    {
+        $this->obj->required = true;
+        try {
+            $this->obj->validate();
+        }
+        catch(ContentValidationException $ex) {
+            $this->assertEquals("Test date is required.", $ex->getMessage());
+        }
+
+        $this->obj->value = null;
+        try {
+            $this->obj->validate();
+        }
+        catch(ContentValidationException $ex) {
+            $this->assertEquals("Test date is required.", $ex->getMessage());
+        }
+
+        $this->obj->value = '';
+        try {
+            $this->obj->validate();
+        }
+        catch(ContentValidationException $ex) {
+            $this->assertEquals("Test date is required.", $ex->getMessage());
+        }
+    }
+
+    public function testValidateInvalidDateFormats()
+    {
+        $this->obj->required = false;
+        $this->obj->value = '32-2-2018';
+        try {
+            $this->obj->validate();
+        }
+        catch (ContentValidationException $ex) {
+            $this->assertEquals("Test date is not in a recognized date format.", $ex->getMessage());
+        }
+
+	    $this->obj->value = 'duygoci';
+	    try {
+		    $this->obj->validate();
+	    }
+	    catch (ContentValidationException $ex) {
+		    $this->assertEquals("Test date is not in a recognized date format.", $ex->getMessage());
+	    }
+    }
+
+    public function testEscapeSQL()
+    {
+	    $mysqli = new \mysqli();
+	    $mysqli->connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_SCHEMA);
+
+    	$this->obj->setInputValue('May 23, 2018');
+    	$this->assertEquals("'2018-05-23 00:00:00'", $this->obj->escapeSQL($mysqli));
+
+	    $this->obj->value = null;
+	    $this->assertEquals('NULL', $this->obj->escapeSQL($mysqli));
+
+	    $this->obj->setInputValue('');
+	    $this->assertEquals('NULL', $this->obj->escapeSQL($mysqli));
+
+	    $this->obj->value = "fdoclxps";
+	    $this->assertEquals("'fdoclxps'", $this->obj->escapeSQL($mysqli));
+    }
+}
