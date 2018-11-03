a9f27519a4e4aea27a56a6d7d31af7403d2bf0fa
Damien Barchowsky <dbarchowsky@gmail.com>
2018-11-03 07:36:25 -0700

    Added commit, cancel, and referer to LittledGlobals

--- lib/Littled/App/LittledGlobals.php
+++ lib/Littled/App/LittledGlobals.php
@@ -6,4 +6,11 @@ class LittledGlobals
 {
 	/** @var string ID request variable name. */
 	const ID_PARAM = 'id';
+
+	/** @var string Request variable name to commit operations. */
+	const P_COMMIT = 'commit';
+	/** @var string Request variable name to cancel operations. */
+	const P_CANCEL = 'cancel';
+	/** @var string Request variable name containing referring URLs. */
+	const P_REFERER = 'ref';
 }
