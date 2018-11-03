aec4b7d3eff5b154fd3f10722db49bcc1e611d3d
Damien Barchowsky <dbarchowsky@gmail.com>
2018-11-03 09:04:47 -0700

    Added LittledGlobals::P_ID const

--- lib/Littled/App/LittledGlobals.php
+++ lib/Littled/App/LittledGlobals.php
@@ -7,6 +7,8 @@ class LittledGlobals
 	/** @var string ID request variable name. */
 	const ID_PARAM = 'id';
 
+	/** @var string Request variable name holding record ids. */
+	const P_ID = 'ID';
 	/** @var string Request variable name to commit operations. */
 	const P_COMMIT = 'commit';
 	/** @var string Request variable name to cancel operations. */
