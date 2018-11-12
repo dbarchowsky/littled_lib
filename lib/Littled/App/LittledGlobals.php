7b1f024e7a5cc9cf6fb71a87be6bcfab434deb6b
Damien Barchowsky <dbarchowsky@gmail.com>
2018-11-11 16:30:21 -0800

    Added LittledGlobals::P_MESSAGE property

--- lib/Littled/App/LittledGlobals.php
+++ lib/Littled/App/LittledGlobals.php
@@ -13,12 +13,14 @@ class LittledGlobals
 
 	/** @var string Request variable name holding record ids. */
 	const P_ID = 'ID';
-	/** @var string Request variable name to commit operations. */
-	const P_COMMIT = 'commit';
 	/** @var string Request variable name to cancel operations. */
 	const P_CANCEL = 'cancel';
+	/** @var string Request variable name to commit operations. */
+	const P_COMMIT = 'commit';
 	/** @var string Request variable flag indicating that listings are being filtered. */
 	const P_FILTER = 'filter';
+	/** @var string Request variable containing status message. */
+	const P_MESSAGE = 'msg';
 	/** @var string Request variable name containing referring URLs. */
 	const P_REFERER = 'ref';
 }
