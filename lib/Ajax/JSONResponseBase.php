bb5e238109ce3bc4fc0c3d246b8f2f4147afbb41
Damien Barchowsky <dbarchowsky@gmail.com>
2021-11-10 20:33:56 -0800

    Reverted parameters types from previous commit

--- lib/Ajax/JSONResponseBase.php
+++ lib/Ajax/JSONResponseBase.php
@@ -1,8 +1,6 @@
 <?php
 namespace Littled\Ajax;
 
-use stdClass;
-
 class JSONResponseBase
 {
     /** @var string */
@@ -19,9 +17,9 @@ class JSONResponseBase
     /**
      * Converts keys and values of the object into an object that will be used to generate a JSON object
      * to be transmitted to another page as an AJAX response.
-     * @return stdClass
+     * @return array
      */
-    public function formatJson(): stdClass
+    public function formatJson(): array
     {
         $arr = array();
         foreach($this as $tag) {
@@ -42,9 +40,9 @@ class JSONResponseBase
 
 	/**
 	 * Sends json data as response to client.
-	 * @param stdClass $arr json data to send as response to client
+	 * @param array $arr json data to send as response to client
 	 */
-	public static function sendJsonResponse(stdClass $arr)
+	public static function sendJsonResponse(array $arr)
 	{
 		header('Content-type: application/json; charset=utf-8');
 		print json_encode($arr, JSON_UNESCAPED_UNICODE|JSON_HEX_TAG);
