02ab7324ad42b4811df1186438002ac3ce581597
Damien Barchowsky <dbarchowsky@gmail.com>
2021-11-15 10:12:56 -0800

    Added return type declaration to AppContentBase::getMethodDescriptor()

--- lib/Database/AppContentBase.php
+++ lib/Database/AppContentBase.php
@@ -14,7 +14,7 @@ class AppContentBase extends MySQLConnection
 	 * Returns the current class base name and method name.
 	 * @return string Class and method description.
 	 */
-	public static function getMethodDescriptor()
+	public static function getMethodDescriptor(): string
 	{
 		return(basename(str_replace('\\', '/', get_called_class()))."::".debug_backtrace()[1]['function']."()");
 	}
