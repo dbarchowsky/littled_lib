b5509a079f71655b64fd4423e9d105cbdd151277
Damien Barchowsky <dbarchowsky@gmail.com>
2022-01-04 03:16:03 -0800

    Added InvalidReqeustException

---
+++ lib/Exception/InvalidRequestException.php
@@ -0,0 +1,33 @@
+<?php
+namespace Littled\Exception;
+
+use Exception;
+
+/**
+ * Class InvalidTypeException
+ * @package Littled\Exception
+ */
+class InvalidRequestException extends \Exception
+{
+	/**
+	 * InvalidTypeException constructor.
+	 * @param string $message Error message.
+	 * @param int[optional] $code Error code.
+	 * @param Exception|null $previous
+	 */
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        // some code
+
+        // make sure everything is assigned properly
+        parent::__construct($message, $code, $previous);
+    }
+
+	/**
+	 * custom string representation of object
+	 */
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
