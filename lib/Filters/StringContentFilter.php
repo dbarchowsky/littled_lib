2a90297086deadfb81b1fe21fd5230f6bbe1cab3
Damien Barchowsky <dbarchowsky@gmail.com>
2022-01-07 15:41:28 -0800

    added StringContentFilter::escapeSQL()

--- lib/Filters/StringContentFilter.php
+++ lib/Filters/StringContentFilter.php
@@ -2,11 +2,23 @@
 namespace Littled\Filters;
 
 
+use mysqli;
+
 /**
  * Class StringContentFilter
  * @package BFHHand\Filters
  */
 class StringContentFilter extends ContentFilter
 {
-
+    public function escapeSQL(mysqli $mysqli, bool $include_quotes=true, bool $include_wildcards=true): string
+    {
+        $quote = ($include_quotes)?("'"):('');
+        $wildcard = ($include_wildcards)?("%"):('');
+        if ($this->value) {
+            return $quote.$wildcard.$mysqli->real_escape_string($this->value).$wildcard.$quote;
+        }
+        else {
+            return $quote.$quote;
+        }
+    }
 }
---
+++ tests/Filters/StringContentFilterTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Littled\Tests\Filters;
+require_once (realpath(dirname(__FILE__)).'/../bootstrap.php');
+
+use Littled\Database\MySQLConnection;
+use Littled\Exception\ConfigurationUndefinedException;
+use Littled\Filters\StringContentFilter;
+use PHPUnit\Framework\TestCase;
+
+class StringContentFilterTest extends TestCase
+{
+    /**
+     * @throws ConfigurationUndefinedException
+     */
+    function testEscapeSQL()
+    {
+        $c = new MySQLConnection();
+
+        // null value
+        $sf = new StringContentFilter('label', 'key', null, 50);
+        $this->assertEquals("''", $sf->escapeSQL($c->getMysqli()));
+        $this->assertEquals("", $sf->escapeSQL($c->getMysqli(), false));
+        $this->assertEquals("", $sf->escapeSQL($c->getMysqli(), false, false));
+
+        // empty string
+        $sf->value = '';
+        $this->assertEquals("''", $sf->escapeSQL($c->getMysqli()));
+        $this->assertEquals("", $sf->escapeSQL($c->getMysqli(), false));
+        $this->assertEquals("", $sf->escapeSQL($c->getMysqli(), false, false));
+
+        // non-empty string
+        $sf->value = 'search pattern';
+        $this->assertEquals("'%search pattern%'", $sf->escapeSQL($c->getMysqli()));
+        $this->assertEquals("%search pattern%", $sf->escapeSQL($c->getMysqli(), false));
+        $this->assertEquals("search pattern", $sf->escapeSQL($c->getMysqli(), false, false));
+        $this->assertEquals("'search pattern'", $sf->escapeSQL($c->getMysqli(), true, false));
+    }
+}
