cebc048d6aaec8a4bdafdbcaba645d69a8fd2208
Damien Barchowsky <dbarchowsky@gmail.com>
2022-01-14 17:26:44 -0800

    Added filter properties to ContentFilters test object

--- tests/Filters/Samples/ContentFiltersProcedureSample.php
+++ tests/Filters/Samples/ContentFiltersProcedureSample.php
@@ -1,28 +1,50 @@
 <?php
 namespace Littled\Tests\Filters\Samples;
 
+use Cassandra\Date;
+use Littled\Filters\DateContentFilter;
+use Littled\Filters\StringContentFilter;
+
 class ContentFiltersProcedureSample extends ContentFiltersSample
 {
+    /** @var StringContentFilter */
+    public $title;
+    /** @var StringContentFilter */
+    public $text;
+    /** @var StringContentFilter */
+    public $source;
+    /** @var DateContentFilter */
+    public $published_after;
+    /** @var DateContentFilter */
+    public $published_before;
+    /** @var StringContentFilter */
+    public $keyword;
+
     public function __construct(int $content_type_id)
     {
         parent::__construct($content_type_id);
         $this->page->value = 1;
         $this->listings_length->value = 4;
+        $this->title = new StringContentFilter('title', 'titleFilter', null, 50);
+        $this->text = new StringContentFilter('text', 'textFilter', null, 50);
+        $this->source = new StringContentFilter('source', 'sourceFilter', null, 50);
+        $this->published_after = new DateContentFilter('published after', 'pubAfter', null);
+        $this->published_before = new DateContentFilter('published before', 'pubBefore', null);
+        $this->keyword = new StringContentFilter('keyword', 'keywordFilter', null, 50);
     }
 
     protected function formatListingsQuery(): array
     {
-        $title_filter = $text_filter = $source_filter = $pub_after = $pub_before = $keyword_filter = null;
         return array(
             "CALL articleListingsSelect(?,?,?,?,?,?,?,?,@total_matches)",
             'iissssss',
             $this->page->value,
             $this->listings_length->value,
-            $title_filter,
-            $text_filter,
-            $source_filter,
-            $pub_after,
-            $pub_before,
-            $keyword_filter);
+            $this->title->value,
+            $this->text->value,
+            $this->source->value,
+            $this->published_after->value,
+            $this->published_before->value,
+            $this->keyword->value);
     }
 }
