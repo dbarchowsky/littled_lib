08d135b938da3e0750f75e428a0153436a3a42fc
Damien Barchowsky <dbarchowsky@gmail.com>
2022-02-18 09:21:35 -0800

    Adjusted formatting in configuration files

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 define('APP_BASE_DIR', realpath(dirname(__FILE__)).'/../');
-const SHARED_CMS_TEMPLATE_DIR = APP_BASE_DIR.'../littled_cms/templates/';
-const LITTLED_TEMPLATE_DIR = SHARED_CMS_TEMPLATE_DIR;
-const TEST_ASSETS_PATH = APP_BASE_DIR.'tests/assets/';
-const TEST_TEMPLATES_PATH = TEST_ASSETS_PATH.'templates/';
-const TEST_HARNESS_BASE_URI = 'http://localhost';
+const SHARED_CMS_TEMPLATE_DIR   = APP_BASE_DIR.'../littled_cms/templates/';
+const LITTLED_TEMPLATE_DIR      = SHARED_CMS_TEMPLATE_DIR;
+const TEST_ASSETS_PATH          = APP_BASE_DIR.'tests/assets/';
+const TEST_TEMPLATES_PATH       = TEST_ASSETS_PATH.'templates/';
+const TEST_HARNESS_BASE_URI     = 'http://localhost';
