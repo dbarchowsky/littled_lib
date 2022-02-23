ad129c9048a46df7b6d52adb219c7efe1086f723
Damien Barchowsky <dbarchowsky@gmail.com>
2022-02-23 05:05:22 -0800

    Added StringContentFilter::collectRequestVar()

--- lib/Filters/StringContentFilter.php
+++ lib/Filters/StringContentFilter.php
@@ -1,8 +1,9 @@
 <?php
 namespace Littled\Filters;
 
-
+use Littled\Validation\Validation;
 use mysqli;
+
 
 /**
  * Class StringContentFilter
@@ -10,6 +11,17 @@ use mysqli;
  */
 class StringContentFilter extends ContentFilter
 {
+    /**
+     * @inheritDoc
+     */
+    protected function collectRequestValue()
+    {
+        $this->value = Validation::collectStringRequestVar($this->key);
+    }
+
+    /**
+     * @inheritDoc
+     */
     public function escapeSQL(mysqli $mysqli, bool $include_quotes=true, bool $include_wildcards=true): string
     {
         $quote = ($include_quotes)?("'"):('');
