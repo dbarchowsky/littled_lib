e5abda61a8fcc9764656844963857d9db3f9dee1
Damien Barchowsky <dbarchowsky@gmail.com>
2022-02-25 10:19:29 -0800

    Set default templates for StringTextarea

--- lib/Request/StringTextarea.php
+++ lib/Request/StringTextarea.php
@@ -4,5 +4,9 @@ namespace Littled\Request;
 
 class StringTextarea extends StringInput
 {
+    /** @var string */
+    protected static $input_template_filename = 'textarea-input.php';
+    /** @var string */
+    protected static $template_filename = 'string-text-field.php';
 
 }
