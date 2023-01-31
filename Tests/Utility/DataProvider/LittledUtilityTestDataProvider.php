958a691abfb0ad4a0df867828e374e4cb3be3224
Damien Barchowsky <dbarchowsky@gmail.com>
2023-01-31 10:43:32 -0800

    Added test case to LittledUtilityTest

--- Tests/Utility/DataProvider/LittledUtilityTestDataProvider.php
+++ Tests/Utility/DataProvider/LittledUtilityTestDataProvider.php
@@ -39,6 +39,7 @@ class LittledUtilityTestDataProvider
 			array('abc/def', array('abc', 'def')),
 			array('abc/def', array('abc', '/def')),
 			array('/abc/def', array('/abc', 'def')),
+			array('/abc/def', array('/abc/', '/def')),
 			array('foo.jpg', array('', 'foo.jpg')),
 			array('dir/0/a.jpg', array('dir', '0', 'a.jpg')),
 		);
