fe0e477f8a8d53246c4172581db875a26a42136c
Damien Barchowsky <dbarchowsky@gmail.com>
2023-03-03 09:37:55 -0800

    Bug fix test for NULL values in udfCalcPageLimits procedure

--- _dbo/functions/udfCalcPageLimits.sql
+++ _dbo/functions/udfCalcPageLimits.sql
@@ -1,21 +1,21 @@
 DELIMITER $$
 
 CREATE OR REPLACE PROCEDURE `udfCalcPageLimits`(
-  IN p_page INT,
-  IN p_page_length INT,
-  OUT p_offset INT,
-  OUT p_limit INT
+    IN p_page INT,
+    IN p_page_length INT,
+    OUT p_offset INT,
+    OUT p_limit INT
 )
-  BEGIN
+BEGIN
 
-    IF p_page > 0 AND p_page_length > 0 THEN
-      SELECT ((p_page-1) * p_page_length) INTO p_offset;
-      SELECT p_page_length INTO p_limit;
+    IF IFNULL(p_page, 0) > 0 AND IFNULL(p_page_length, 0) > 0 THEN
+        SELECT ((p_page-1) * p_page_length) INTO p_offset;
+        SELECT p_page_length INTO p_limit;
     ELSE
-      SELECT 0 INTO p_offset;
-      SELECT 2147483647 INTO p_limit;
+        SELECT 0 INTO p_offset;
+        SELECT 2147483647 INTO p_limit;
     END if;
 
-  END $$
+END $$
 
 DELIMITER ;
