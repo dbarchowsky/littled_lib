c71aafff6a8c5d3415239762c1de42262b125c9a
Damien Barchowsky <dbarchowsky@gmail.com>
2024-01-19 03:04:18 -0800

    Added class Request\ForeignKeyInput

---
+++ Tests/Request/ForeignKeyInputTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace LittledTests\Request;
+
+
+use Littled\Exception\ConfigurationUndefinedException;
+use Littled\Exception\InvalidTypeException;
+use Littled\PageContent\Serialized\LinkedContent;
+use Littled\Request\ForeignKeyInput;
+use LittledTests\TestHarness\PageContent\Serialized\LinkedContentTestHarness;
+use PHPUnit\Framework\TestCase;
+
+class ForeignKeyInputTest extends TestCase
+{
+    public function testGetContentUninitialized()
+    {
+        $o = new ForeignKeyInput('My label', 'testKey');
+
+        try {
+            $class = $o->getContentClass();
+            self::fail('Expected ConfigurationUndefinedException not thrown.');
+        }
+        catch (ConfigurationUndefinedException $e) {
+            self::assertMatchesRegularExpression('/content class.* not.* assigned/i', $e->getMessage());
+        }
+    }
+
+    public function testIsDatabaseFieldDefault()
+    {
+        $o = new ForeignKeyInput('DB field default', 'testKey');
+        self::assertFalse($o->isDatabaseField());
+    }
+
+    /**
+     * @throws InvalidTypeException
+     * @throws ConfigurationUndefinedException
+     */
+    public function testSetContentClass()
+    {
+        $o = new ForeignKeyInput('My label', 'testKey');
+        $o->setContentClass(LinkedContentTestHarness::class);
+        self::assertEquals(LinkedContentTestHarness::class, $o->getContentClass());
+    }
+
+    /**
+     * @throws InvalidTypeException
+     * @throws ConfigurationUndefinedException
+     */
+    public function testSetContentClassChained()
+    {
+        $o = (new ForeignKeyInput('My label', 'testKey'))
+            ->setContentClass(LinkedContentTestHarness::class);
+        self::assertEquals(LinkedContentTestHarness::class, $o->getContentClass());
+    }
+
+    public function testSetContentClassWithBadClass()
+    {
+        $o = new ForeignKeyInput('My label', 'testKey');
+
+        try {
+            $o->setContentClass('GarbageClass');
+            self::fail('Expected InvalidTypeException not thrown.');
+        }
+        catch (InvalidTypeException $e) {
+            $expected = '/content class.* '.str_replace('\\', '\\\\', LinkedContent::class).'/i';
+            self::assertMatchesRegularExpression($expected, $e->getMessage());
+        }
+    }
+}
---
+++ lib/Request/ForeignKeyInput.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Littled\Request;
+
+
+use Littled\Exception\ConfigurationUndefinedException;
+use Littled\Exception\InvalidTypeException;
+use Littled\PageContent\Serialized\LinkedContent;
+use Littled\Validation\Validation;
+
+/**
+ * Input class designed to collect and commit foreign key input.
+ */
+class ForeignKeyInput extends IntegerSelect
+{
+    protected string $content_class;
+
+    public function __construct(
+        string  $label,
+        string  $key,
+        bool    $required = false,
+                $value = null,
+        int     $size_limit = 0,
+        ?int    $index = null)
+    {
+        parent::__construct($label, $key, $required, $value, $size_limit, $index);
+        $this->setIsDatabaseField(false);
+    }
+
+    /**
+     * Content class getter.
+     * @return string
+     */
+    public function getContentClass():string
+    {
+        if (!isset($this->content_class)) {
+            throw new ConfigurationUndefinedException(
+                'The content class of the foreign key input has not been assigned.');
+        }
+        return $this->content_class;
+    }
+
+    /**
+     * Content class setter.
+     * @param string $class
+     * @return void
+     * @throws InvalidTypeException
+     */
+    public function setContentClass(string $class): ForeignKeyInput
+    {
+        if (!Validation::isSubclass($class, LinkedContent::class)) {
+            throw new InvalidTypeException(
+                'Content class for foreign keys must be of type '.LinkedContent::class.'.');
+        }
+        $this->content_class = $class;
+        return $this;
+    }
+}
