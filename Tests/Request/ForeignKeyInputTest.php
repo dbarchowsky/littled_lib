a6e0f7c29d7f5aff503a07f89e9cc0ad41c2bfc7
Damien Barchowsky <dbarchowsky@gmail.com>
2024-01-22 13:42:18 -0800

    Refactored Request\ForeignKeyInput to remove $content_class property and its getter/setter routines

--- Tests/Request/ForeignKeyInputTest.php
+++ Tests/Request/ForeignKeyInputTest.php
@@ -12,69 +12,9 @@ use PHPUnit\Framework\TestCase;
 
 class ForeignKeyInputTest extends TestCase
 {
-    public function testGetContentUninitialized()
-    {
-        $o = new ForeignKeyInput('My label', 'testKey');
-
-        try {
-            $class = $o->getContentClass();
-            self::fail('Expected ConfigurationUndefinedException not thrown.');
-        }
-        catch (ConfigurationUndefinedException $e) {
-            self::assertMatchesRegularExpression('/content class.* not.* assigned/i', $e->getMessage());
-        }
-    }
-
     public function testIsDatabaseFieldDefault()
     {
         $o = new ForeignKeyInput('DB field default', 'testKey');
         self::assertTrue($o->isDatabaseField());
-    }
-
-    /**
-     * @throws InvalidTypeException
-     * @throws ConfigurationUndefinedException
-     */
-    public function testSetContentClass()
-    {
-        $o = new ForeignKeyInput('My label', 'testKey');
-        $o->setContentClass(LinkedContentTestHarness::class);
-        self::assertEquals(LinkedContentTestHarness::class, $o->getContentClass());
-    }
-
-    /**
-     * @throws InvalidTypeException
-     * @throws ConfigurationUndefinedException
-     */
-    public function testSetContentClassChained()
-    {
-        $o = (new ForeignKeyInput('My label', 'testKey'))
-            ->setContentClass(LinkedContentTestHarness::class);
-        self::assertEquals(LinkedContentTestHarness::class, $o->getContentClass());
-    }
-
-    public function testSetContentClassWithBadClass()
-    {
-        $o = new ForeignKeyInput('My label', 'testKey');
-
-        try {
-            $o->setContentClass('GarbageClass');
-            self::fail('Expected InvalidTypeException not thrown.');
-        }
-        catch (InvalidTypeException $e) {
-            $expected = '/content class.* '.str_replace('\\', '\\\\', LinkedContent::class).'/i';
-            self::assertMatchesRegularExpression($expected, $e->getMessage());
-        }
-    }
-
-    public function testSetLinkedListingsCB()
-    {
-        $o = new ForeignKeyInput('FK input', 'fkKey');
-        self::assertFalse($o->getLinkedListingsCB());
-
-        $cb = 'linkedListingsSelect';
-        $o2 = $o->setLinkedListingsCB($cb);
-        self::assertEquals($cb, $o->getLinkedListingsCB());
-        self::assertEquals($cb, $o2->getLinkedListingsCB());
     }
 }
--- lib/Request/ForeignKeyInput.php
+++ lib/Request/ForeignKeyInput.php
@@ -3,70 +3,9 @@
 namespace Littled\Request;
 
 
-use Littled\Exception\ConfigurationUndefinedException;
-use Littled\Exception\InvalidTypeException;
-use Littled\PageContent\Serialized\LinkedContent;
-use Littled\Validation\Validation;
-
 /**
  * Input class designed to collect and commit foreign key input.
  */
 class ForeignKeyInput extends IntegerSelect
 {
-    protected string $content_class;
-
-    protected string $linked_lisings_cb;
-
-    /**
-     * Content class getter.
-     * @return string
-     * @throws ConfigurationUndefinedException
-     */
-    public function getContentClass():string
-    {
-        if (!isset($this->content_class)) {
-            throw new ConfigurationUndefinedException(
-                'The content class of the foreign key input has not been assigned.');
-        }
-        return $this->content_class;
-    }
-
-    /**
-     * Linked listings callback getter
-     * @return false|string
-     */
-    public function getLinkedListingsCB()
-    {
-        if (isset($this->linked_lisings_cb)) {
-            return $this->linked_lisings_cb;
-        }
-        return false;
-    }
-
-    /**
-     * Content class setter.
-     * @param string $class
-     * @return ForeignKeyInput
-     * @throws InvalidTypeException
-     */
-    public function setContentClass(string $class): ForeignKeyInput
-    {
-        if (!Validation::isSubclass($class, LinkedContent::class)) {
-            throw new InvalidTypeException(
-                'Content class for foreign keys must be of type '.LinkedContent::class.'.');
-        }
-        $this->content_class = $class;
-        return $this;
-    }
-
-    /**
-     * Linked listings callback setter
-     * @param string $procedure Name of database procedure to run to retrieve list of records linked to the parent.
-     * @return ForeignKeyInput
-     */
-    public function setLinkedListingsCB(string $procedure): ForeignKeyInput
-    {
-        $this->linked_lisings_cb = $procedure;
-        return $this;
-    }
 }
