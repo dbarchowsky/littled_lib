f340fe65b630651674c4917e4b267e63ed3bcdd0
Damien Barchowsky <dbarchowsky@gmail.com>
2024-02-09 14:18:49 -0800

    Added Utility\LittledUtility::replaceFirstOccurrence()

--- lib/Utility/LittledUtility.php
+++ lib/Utility/LittledUtility.php
@@ -1,38 +1,46 @@
 <?php
+
 namespace Littled\Utility;
 
+/**
+ * General utility functions.
+ */
 class LittledUtility
 {
-	/**
+    /**
      * @deprecated Use LittledUtility::joinPaths() instead.
-	 * Joins parts of a filesystem path into a single path.
-	 * @param string[] $parts
-	 * @return string
-	 */
-	public static function joinPathParts(array $parts): string
-	{
-		$last = (DIRECTORY_SEPARATOR===(substr(end($parts), -1))?(DIRECTORY_SEPARATOR):(''));
-		reset($parts);
-		$parts = array_map(function($i) { return trim($i, DIRECTORY_SEPARATOR); }, array_filter($parts));
-		if (0 < count($parts)) {
-			return DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts).$last;
-		}
-		return '';
-	}
+     * Joins parts of a filesystem path into a single path.
+     * @param string[] $parts
+     * @return string
+     */
+    public static function joinPathParts(array $parts): string
+    {
+        $last = (DIRECTORY_SEPARATOR === (substr(end($parts), -1)) ? (DIRECTORY_SEPARATOR) : (''));
+        reset($parts);
+        $parts = array_map(function ($i) {
+            return trim($i, DIRECTORY_SEPARATOR);
+        }, array_filter($parts));
+        if (0 < count($parts)) {
+            return DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts) . $last;
+        }
+        return '';
+    }
 
-	/**
-	 * Joins variable length list of strings into a single filesystem path strong. Unlink LittledUtility::joinPathParts(),
-	 * it will not add a leading slash to the path if it isn't present in the first string passed to the method.
-	 * @return string
-	 */
-	public static function joinPaths(): string
-	{
-		$paths = array();
-		foreach (func_get_args() as $arg) {
-			if ($arg !== '') { $paths[] = $arg; }
-		}
-		return preg_replace('#/+#','/',join('/', $paths));
-	}
+    /**
+     * Joins variable length list of strings into a single filesystem path strong. Unlink LittledUtility::joinPathParts(),
+     * it will not add a leading slash to the path if it isn't present in the first string passed to the method.
+     * @return string
+     */
+    public static function joinPaths(): string
+    {
+        $paths = array();
+        foreach (func_get_args() as $arg) {
+            if ($arg !== '') {
+                $paths[] = $arg;
+            }
+        }
+        return preg_replace('#/+#', '/', join('/', $paths));
+    }
 
     public static function overlap(string $a, string $b)
     {
@@ -51,6 +59,23 @@ class LittledUtility
     }
 
     /**
+     * Replaces the first occurrence of $needle in $haystack with $replace. Returns the original string if no matches
+     * are found.
+     * @param string $needle
+     * @param string $replace
+     * @param string $haystack
+     * @return array|string|string[]
+     */
+    public static function replaceFirstOccurrence(string $needle, string $replace, string $haystack)
+    {
+        $pos = strpos($haystack, $needle);
+        if ($pos !== false) {
+            return substr_replace($haystack, $replace, $pos, strlen($needle));
+        }
+        return $haystack;
+    }
+
+    /**
      * Strips levels off a filesystem path.
      * @param string $path Path to edit.
      * @param int $levels Number of directories to remove from the end of the path.
@@ -62,7 +87,7 @@ class LittledUtility
         if (count($parts) < $levels) {
             return '';
         }
-        $parts = array_splice($parts, 0, (-1*$levels));
-        return DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $parts).DIRECTORY_SEPARATOR;
+        $parts = array_splice($parts, 0, (-1 * $levels));
+        return DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts) . DIRECTORY_SEPARATOR;
     }
 }
