5ddeec4cfd2915b45e044d3cc64ccd6ef6e55710
Damien Barchowsky <dbarchowsky@gmail.com>
2025-12-28 14:59:57 -0800

    Added \Exception\ResponseException

---
+++ lib/Exception/ResponseException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Littled\Exception;
+
+/**
+ * Intended to be used by API classes to indicate that a response should be returned.
+ */
+class ResponseException extends LittledException
+{
+}
