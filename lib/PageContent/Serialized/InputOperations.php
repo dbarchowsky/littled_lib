d02892764d1f37e1ce46df1dc44c762c81c8f3c8
Damien Barchowsky <dbarchowsky@gmail.com>
2026-01-31 07:27:49 -0800

    Added InputOperations::applyLabelPrefix and restoreLabelPrefix methods

--- lib/PageContent/Serialized/InputOperations.php
+++ lib/PageContent/Serialized/InputOperations.php
@@ -29,17 +29,49 @@ trait InputOperations
     /**
      * Prepends $prefix to the label of each RequestInput property of the object.
      * @param string $prefix
+     * @param array $exclude_keys
      * @return SerializedContentUtils
      */
-    public function applyLabelPrefix(string $prefix): static
+    public function applyLabelPrefix(string $prefix, array $exclude_keys=[]): static
     {
-        $ip = $this->getInputPropertiesList(false);
+        $ip = array_unique([...$this->getKeyPropertiesList(), ...$this->getInputPropertiesList(false)]);
         foreach($ip as $property) {
-            $this->$property->label = trim($prefix) . ' ' . $this->$property->label;
+            if (in_array($this->$property->key, $exclude_keys)) {
+                continue;
+            }
+            $this->$property->label = ucfirst(strtolower(trim($prefix) . ' ' . $this->$property->label));
+            $exclude_keys[] = $this->$property->key;
         }
         $cp = $this->getContentPropertiesList();
         foreach($cp as $property) {
-            $this->$property->applyLabelPrefix($prefix);
+            $this->$property->applyLabelPrefix($prefix, $exclude_keys);
+        }
+        return $this;
+    }
+
+    /**
+     * Prepends $prefix to the label of each RequestInput property of the object.
+     * @param string $prefix
+     * @param array $exclude_keys
+     * @return SerializedContentUtils
+     */
+    public function stripLabelPrefix(string $prefix, array $exclude_keys = []): static
+    {
+        $ip = array_unique([...$this->getKeyPropertiesList(), ...$this->getInputPropertiesList(false)]);
+        foreach($ip as $property) {
+            if (in_array($this->$property->key, $exclude_keys)) {
+                continue;
+            }
+            $label = $this->$property->label;
+            if (str_starts_with($label, $prefix)) {
+                $label = substr($label, strlen($prefix));
+            }
+            $this->$property->label = ucfirst(strtolower(ltrim($label)));
+            $exclude_keys[] = $this->$property->key;
+        }
+        $cp = $this->getContentPropertiesList();
+        foreach($cp as $property) {
+            $this->$property->stripLabelPrefix($prefix, $exclude_keys);
         }
         return $this;
     }
