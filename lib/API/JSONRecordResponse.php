18b6cdaf6cae091091116927c28d8e8bc9dbe462
Damien Barchowsky <dbarchowsky@gmail.com>
2026-02-06 12:03:17 -0800

    Refactored JSONRecordResponse to add method setResponseLabel()

--- lib/API/JSONRecordResponse.php
+++ lib/API/JSONRecordResponse.php
@@ -85,6 +85,12 @@ class JSONRecordResponse extends JSONResponse
             ->setResponseStatus($status);
     }
 
+    public function setResponseLabel(string $label): JSONRecordResponse
+    {
+        $this->label->value = $label;
+        return $this;
+    }
+
     /**
      * Chainable response status value setter.
      * @param string $status
