46cde9eb9abeba1df98524617e30d3a593354ce1
Damien Barchowsky <dbarchowsky@gmail.com>
2026-03-09 17:15:40 -0700

    Added method PageScript::getViteAsset() to resolve versioned asset paths with Vite manifest integration

--- lib/PageContent/PageScript.php
+++ lib/PageContent/PageScript.php
@@ -2,6 +2,10 @@
 
 namespace Littled\PageContent;
 
+use Littled\App\AppBase;
+use Littled\App\LittledGlobals;
+use Littled\Exception\ConfigurationUndefinedException;
+
 class PageScript
 {
     public string $script;
@@ -15,6 +19,43 @@ class PageScript
     public function getType(): string
     {
         return $this->type ?? '';
+    }
+
+    /**
+     * Resolves a versioned asset path from the Vite manifest.
+     * @return string
+     * @throws ConfigurationUndefinedException
+     */
+    public function getViteAsset(): string
+    {
+        $isDev = AppBase::getAppEnv() === 'development';
+        $assetPath = 'app/js/';
+        $devHost = 'http://localhost:5173';
+
+        $fullPath = $assetPath . $this->script;
+        if ($isDev) {
+            return "$devHost/$fullPath";
+        }
+
+        if (!file_exists(static::getViteManifestPath())) {
+            return "/dist/$fullPath";
+        }
+        $manifest = json_decode(file_get_contents(static::getViteManifestPath()), true);
+
+        if (isset($manifest[$fullPath])) {
+            return '/dist/' . $manifest[$fullPath]['file'];
+        }
+        return "/dist/$fullPath";
+    }
+
+    /**
+     * Path to the Vite manifest file.
+     * @return string
+     * @throws ConfigurationUndefinedException
+     */
+    protected static function getViteManifestPath(): string
+    {
+        return LittledGlobals::getAppBaseDir() . 'dist/.vite/manifest.json';
     }
 
     public function setScript(string $script): static
