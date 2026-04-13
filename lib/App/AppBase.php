125e15132af46014b2c2a82e94b3b290fda1b73d
Damien Barchowsky <dbarchowsky@gmail.com>
2026-04-13 09:11:00 -0700

    Added AppBase::generateUuidV4() for generating random UUIDs

--- lib/App/AppBase.php
+++ lib/App/AppBase.php
@@ -64,6 +64,25 @@ class AppBase
     }
 
     /**
+     * Generates unique strings to use as identifier tokens.
+     * @return string
+     * @throws RandomException
+     */
+    public static function generateUuidV4(): string
+    {
+        // Generate 16 bytes (128 bits) of random data
+        $data = random_bytes(16);
+
+        // Set the version to 0100 (4) - standard for random UUIDs
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10 - the variant
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Format into the 8-4-4-4-12 string format
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    /**
      * Returns the stream currently being used as the source of ajax client request data.
      * @return string
      */
