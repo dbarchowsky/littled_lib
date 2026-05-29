5b679e921c51ea3c24f8c78028a151f716189a0c
Damien Barchowsky <dbarchowsky@gmail.com>
2026-05-29 06:01:01 -0700

    Added `ContentMap` and `ContentRegistry` classes

---
+++ lib/PageContent/ContentMap.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Littled\PageContent;
+
+
+class ContentMap
+{
+    public int          $id;
+    public string       $slug;
+    public string       $class;
+
+    function __construct(?int $id=null, string $name='', string $class='')
+    {
+        if ($id) {
+            $this->id = $id;
+        }
+        if ($name) {
+            $this->slug = $name;
+        }
+        if ($class) {
+            $this->class = $class;
+        }
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
+    public function setClass(string $class): static
+    {
+        $this->class = $class;
+        return $this;
+    }
+}
---
+++ lib/PageContent/ContentRegistry.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Littled\PageContent;
+
+
+class ContentRegistry
+{
+    public static array $registry = [];
+
+    /**
+     * Returns the class name for a content type.
+     * @param string|int $type_id
+     * @return string
+     */
+    public static function getClass(string|int $type_id): string
+    {
+        if (is_numeric($type_id)) {
+            $map = static::lookupById((int)$type_id);
+        } else {
+            $map = static::lookup($type_id);
+        }
+        return $map->class ?? '';
+    }
+
+    /**
+     * Returns a content map by name.
+     * @param string $name
+     * @return ContentMap|null
+     */
+    public static function lookup(string $name): ?ContentMap
+    {
+        return static::$registry[$name] ?? null;
+    }
+
+    /**
+     * Returns a content map by id.
+     * @param int $type_id
+     * @return ContentMap|null
+     */
+    public static function lookupById(int $type_id): ?ContentMap
+    {
+        foreach (static::$registry as $map) {
+            if ($map->id === $type_id) {
+                return $map;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Registers a content map.
+     * @param ContentMap $map
+     * @return void
+     */
+    public static function register(ContentMap $map): void
+    {
+        static::$registry[$map->slug] = $map;
+    }
+}
